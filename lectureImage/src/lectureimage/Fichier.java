76c477cce4ac1404db8e4ff8a08a4b5ea9a3c956
BobyLaMop <simonsays_1092@hotmail.com>
2018-02-07 11:10:19 -0500

    Merge origin/master
    Conflicts:
    	lectureImage/src/lectureimage/LectureImage.java

--- lectureImage/src/lectureimage/Fichier.java
+++ lectureImage/src/lectureimage/Fichier.java
@@ -5,11 +5,41 @@
  */
 package lectureimage;
 
+import java.io.File;
+
 /**
  *
  * @author Simon
  */
 public class Fichier
 {
+    String type;
+    File fichier;
+
+    public Fichier(String type, File fichier)
+    {
+        this.type = type;
+        this.fichier = fichier;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public File getFichier()
+    {
+        return fichier;
+    }
+
+    public void setFichier(File fichier)
+    {
+        this.fichier = fichier;
+    }
     
 }
