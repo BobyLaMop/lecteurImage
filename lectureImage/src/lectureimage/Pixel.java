5776a330b5bf3db95a6c0bc1a57ce87d3964144b
Gelato <pepinpl@hotmail.com>
2018-02-07 10:45:26 -0500

    COOL

---
+++ lectureImage/src/lectureimage/Pixel.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lectureimage;
+
+/**
+ *
+ * @author Christopher True
+ */
+public class Pixel 
+{
+    int rouge;
+    int vert;
+    int bleu;
+
+    public int getRouge() 
+    {
+        return rouge;
+    }
+
+    public void setRouge(int rouge) 
+    {
+        this.rouge = rouge;
+    }
+
+    public int getVert() 
+    {
+        return vert;
+    }
+
+    public void setVert(int vert) 
+    {
+        this.vert = vert;
+    }
+
+    public int getBleu() 
+    {
+        return bleu;
+    }
+
+    public void setBleu(int bleu) 
+    {
+        this.bleu = bleu;
+    }
+    
+}
